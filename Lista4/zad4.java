59ff7f2ba3acb5aed76af504cea7cbeae4e9d866
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-02-26 14:54:51 +0100

    Refactor zadanie 4 lista 4

--- Lista4/zad4.java
+++ Lista4/zad4.java
@@ -1,21 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static List<Integer> evenPositiveSquare(List<Integer> lst) {
-        List<Integer> result = new ArrayList<>();
-
-        int counter = 0;
-
-        for (int num : lst) {
-            if (counter % 2 != 0 && num > 0) {
-                result.add(num*num);
-            }
-            counter++;
-        }
-
-        return result;
+        return lst.stream()
+                .filter(e -> lst.indexOf(e) % 2 != 0 && e > 0)
+                .map(e -> e*e)
+                .toList();
     }
 
     public static void main(String[] args) {
